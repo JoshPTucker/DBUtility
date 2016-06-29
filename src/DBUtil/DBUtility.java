b615daf49c81d5964ba2fe1b90bff7ade5777751
Josh T <jptucker2009@gmail.com>
2016-06-29 16:50:51 -0400

    More functionality added

--- src/DBUtil/DBUtility.java
+++ src/DBUtil/DBUtility.java
@@ -3,6 +3,7 @@ package DBUtil;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -111,5 +112,76 @@ public class DBUtility {
 				e.printStackTrace();
 			}
 		}
+	}
+	public static int getNumberOfCol(String table){
+		
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+
+		String sql ="SELECT * FROM "+table;
+		String printStatement = "Still not working";
+		int nc=0;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			// con = DriverManager.getConnection("jdbc:oracle:thin:sys as
+			// sysdba/oracle@localhost:1521:orcl");
+			con = DriverManager.getConnection("jdbc:oracle:thin:ora1/ora1@localhost:1521:orcl");
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(sql);
+			ResultSetMetaData rsmd = rs.getMetaData();
+			 nc = rsmd.getColumnCount();
+		} catch (SQLException e) {
+			System.err.println("You have enetered an invalid sql statement");
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			System.err.println("Data you are searching for is unavailable");
+			e.printStackTrace();
+		} finally {
+			try {
+				rs.close();
+				stmt.close();
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return nc;
+	}
+	public static String getColNames(String table){
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+
+		String sql ="SELECT * FROM "+table;
+		String printStatement = "Still not working";
+	
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			// con = DriverManager.getConnection("jdbc:oracle:thin:sys as
+			// sysdba/oracle@localhost:1521:orcl");
+			con = DriverManager.getConnection("jdbc:oracle:thin:ora1/ora1@localhost:1521:orcl");
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(sql);
+			ResultSetMetaData rsmd = rs.getMetaData();
+			for(int i=0;i<rsmd.getColumnCount();i++){
+			 printStatement +=" "+ rsmd.getColumnName(0);
+			}
+		} catch (SQLException e) {
+			System.err.println("You have enetered an invalid sql statement");
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			System.err.println("Data you are searching for is unavailable");
+			e.printStackTrace();
+		} finally {
+			try {
+				rs.close();
+				stmt.close();
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return printStatement;
 	}
 }
