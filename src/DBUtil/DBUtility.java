25454cfffefe11dfb0e55fda5cb07ebcff194c2f
Josh T <jptucker2009@gmail.com>
2016-06-30 10:05:27 -0400

    added functin to run sql

--- src/DBUtil/DBUtility.java
+++ src/DBUtil/DBUtility.java
@@ -184,4 +184,39 @@ public class DBUtility {
 		}
 		return printStatement;
 	}
+	public static void runSQL(String sqlU){
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+
+		String sql =sqlU;
+		String printStatement="";
+	
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			// con = DriverManager.getConnection("jdbc:oracle:thin:sys as
+			// sysdba/oracle@localhost:1521:orcl");
+			con = DriverManager.getConnection("jdbc:oracle:thin:ora1/ora1@localhost:1521:orcl");
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(sql);
+			while(rs.next()){
+				for (int i=0;i<rs.getFetchSize();i++)
+				System.out.println(rs.getString(i));
+			}
+		} catch (SQLException e) {
+			System.err.println("You have enetered an invalid sql statement");
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			System.err.println("Data you are searching for is unavailable");
+			e.printStackTrace();
+		} finally {
+			try {
+				rs.close();
+				stmt.close();
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
